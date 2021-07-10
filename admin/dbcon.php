72da1b99fcf0eff0737bc92394d949b0245fb739
Mezbah09 <hmezbah@gmail.com>
2021-07-11 05:12:50 +0600

    ok

--- admin/dbcon.php
+++ admin/dbcon.php
@@ -1,4 +1,11 @@
 <?php 
 $link=mysqli_connect("localhost","root","","book_store");
+/*
+if($link){
+    echo'yes';
+
+}else{
+    echo 'no';
+}*/
 
 ?>
--- admin/login.php
+++ admin/login.php
@@ -1,5 +1,38 @@
 <?php 
 require_once'./dbcon.php';
+session_start();
+
+if(isset($_SESSION['user_login'])){
+  header('location:index.php');
+  }
+
+
+if(isset($_POST['Login'])){
+  $username= $_POST['username'];
+    $password= $_POST['password'];
+
+  $username_check=mysqli_query($link,"SELECT * FROM `user` WHERE `username`='$username'");
+  if(mysqli_num_rows($username_check) >0){
+    $row =mysqli_fetch_assoc($username_check);
+
+    if($row['password']==md5($password)){
+      if($row['status']=='active'){
+        $_SESSION['user_login']=$username;
+        header('location:index.php');
+      } else{
+        $status_inactive="Your status is inactive";
+      }
+    } else{
+      $wrong_password ="This password is wrong";
+    }
+
+  
+  }else{
+    $username_not_found="This username are not found";
+  }
+
+}
+ 
 ?>
 
 <!doctype html>
@@ -24,18 +57,26 @@ require_once'./dbcon.php';
         <h2 class="text-center">Admin Login Form</h2>
             <form action="login.php" method="POST">
                 <div>
-                    <input type="text" placeholder="Username" name="Username" required="" class="form-control" />
+                    <input type="text" placeholder="username" name="username" required="" value="<?php if(isset($username)){echo $username;}?>"  class="form-control" />
                 </div>
                 <div>
-                    <input type="password" placeholder="Password" name="Password" required="" class="form-control" />
+                    <input type="password" placeholder="password" name="password" required="" value="<?php if(isset($password)){echo $password;}?>" class="form-control" />
                 </div>
                 <br/>
+                
                 <div class="d-grid gap-2 d-md-flex justify-content-md-end">
                     <input type="submit" value="Login" name="Login" class="btn btn-dark">
                 </div>
             </form>
         </div>
     </div>
+    <br>
+    <?php if(isset($username_not_found)){
+                  echo '<div class="alert alert-danger col-md-4 offset-md-4">'.$username_not_found.'</div>';} ?>
+        <?php if(isset($wrong_password)){
+                  echo '<div class="alert alert-danger col-md-4 offset-md-4">'.$wrong_password.'</div>';} ?>   
+          <?php if(isset($status_inactive)){
+                  echo '<div class="alert alert-danger col-md-4 offset-md-4">'.$status_inactive.'</div>';} ?>        
     </div>
 
     <!-- Optional JavaScript; choose one of the two! -->
